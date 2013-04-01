b447da0a9d85d4061c2b6a5008765e31638813b2
Keesjan de Vries <keesjan777@gmail.com>
2013-04-01 18:14:39 +0100

    testing retrieve and create functions

--- create_db.py
+++ create_db.py
@@ -2,27 +2,40 @@
 # -*- coding: utf-8 -*-
 
 import sqlite3 , sys, random, os, argparse
-
-Ninput=5
-Noutput=50
-Npoints=10000
+import numpy as np
 
 con = None
 def parse_args():
     parser = argparse.ArgumentParser(description='Creating an sql data base')
+    parser.add_argument('--n-inputs', dest='n_inputs', action='store',type=int, 
+            default=7,  help='Number of input parameters')
+    parser.add_argument('--n-outputs', dest='n_outputs', action='store',type=int, 
+            default=190,  help='Number of output parameters')
+    parser.add_argument('--n-points', '-n',dest='n_points', action='store',type=int, 
+            default=10000,  help='Number of points')
+    parser.add_argument('--collection-id', dest='collection_id', action='store',type=int, 
+            default=1,  help='collection-id')
     parser.add_argument('--output-file','-o', dest='output_file', action='store', 
             default='test.db',  help='Name of output date base file')
-    parser.add_argument('--recreate', dest='recreate', action='store_true', 
+    parser.add_argument('--update', dest='update', action='store_true', 
             default=False,  help='Recreate data base, i.e. delete existing file if exists')
     return parser.parse_args()
 
 args= parse_args()
 
-if args.recreate:
-    os.remove()
+if not args.update:
+    try:
+        os.remove(args.output_file)
+    except FileNotFoundError:
+        pass
 
 # the real stuff
 try:
+    #set numbers
+    Ninput=args.n_inputs
+    Noutput=args.n_outputs
+    Npoints=args.n_points
+    collection_id=args.collection_id
     #connection
     con=sqlite3.connect(args.output_file)
     #cursor
@@ -30,7 +43,7 @@ try:
     input_string=','.join([' in{} REAL '.format(i) for i in range(1,Ninput+1)])
     output_string=','.join([' out{} REAL '.format(i) for i in range(1,Noutput+1)])
     cur.execute('CREATE TABLE IF NOT EXISTS Points(Id INT, CollectionId INT, {},{}) '.format(input_string,output_string))
-    many_in_output=tuple([ tuple([1,2]+[ random.random() for i in range(Ninput+Noutput)]) for j in range(Npoints)])
+    many_in_output=tuple([ tuple(np.append([j,collection_id], np.random.rand(Ninput+Noutput))) for j in range(Npoints)])
     questionmarks= ','.join(['?' for i in range(Ninput+Noutput+2)])  # ?, ... , ?
 #    cur.execute('INSERT INTO Points VALUES({}) '.format(questionmarks),one_in_output)
     cur.executemany('INSERT INTO Points VALUES({}) '.format(questionmarks),many_in_output)
--- retrieve_db.py
+++ retrieve_db.py
@@ -1,30 +1,38 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sqlite3 
-import sys
+import sqlite3 , argparse, sys
 import numpy as np
 
 
+def parse_args():
+    parser = argparse.ArgumentParser(description='Quering an sql database')
+    parser.add_argument('--input-file','-i', dest='input_file', action='store', 
+            default='test.db',  help='Name of output date base file')
+    return parser.parse_args()
 
-def retrieve_collum_between(filename='test.db',column='in1',lower=0.99,upper=1.0)
-    con = None
-    # the real stuff
-    try:
-        #connection
-        con=sqlite3.connect(filename)
-        #cursor
-        cur=con.cursor()
-        #fetch the rows
-        cur.execute('SELECT in1 FROM Points WHERE in1 > {} AND in1<={} '.format(low_edge,high_edge))
-        rows=cur.fetchall()
-    # Finalise ...
-    except sqlite3.Error as e:
-        if con:
-            con.rollback()
-        print('ERROR: {}'.format(e.args[0]))
-        sys.exit()
-    finally:
-        if con:
-            con.close()
-    return rows
+args=parse_args()
+
+filename=args.input_file
+
+con = None
+# the real stuff
+try:
+    #connection
+    con=sqlite3.connect(filename)
+    #cursor
+    cur=con.cursor()
+    #fetch the rows
+#    cur.execute('SELECT COUNT(*) FROM Points  ')
+    cur.execute('SELECT * FROM points where CollectionId=5 and in1 <0.5 and in1> 0.3 and in2<0.001')
+    print(cur.fetchmany(size=5))
+
+# Finalise ...
+except sqlite3.Error as e:
+    if con:
+        con.rollback()
+    print('ERROR: {}'.format(e.args[0]))
+    sys.exit()
+finally:
+    if con:
+        con.close()
