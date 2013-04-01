9c32a78e8057c85467e90ce4476be2b55f9758f3
Keesjan de Vries <keesjan777@gmail.com>
2013-04-01 14:53:14 +0100

    make retrieve into a function

--- retrieve_db.py
+++ retrieve_db.py
@@ -5,34 +5,26 @@ import sqlite3
 import sys
 import numpy as np
 
-con = None
 
-lower=0.99
-upper=0.992
-stepsize=0.001
 
-# the real stuff
-try:
-    #connection
-    con=sqlite3.connect('test.db')
-    #cursor
-    cur=con.cursor()
-    vals=[]
-    for low_edge in np.arange(0.9,1.0,0.01): 
-        high_edge=low_edge+stepsize
-        cur.execute('SELECT in1, in2  FROM Points WHERE in1 > {} AND in1<={} '.format(low_edge,high_edge))
+def retrieve_collum_between(filename='test.db',column='in1',lower=0.99,upper=1.0)
+    con = None
+    # the real stuff
+    try:
+        #connection
+        con=sqlite3.connect(filename)
+        #cursor
+        cur=con.cursor()
+        #fetch the rows
+        cur.execute('SELECT in1 FROM Points WHERE in1 > {} AND in1<={} '.format(low_edge,high_edge))
         rows=cur.fetchall()
-        print(rows)
-        print('==============================')
-
-    print(vals)
-
-# Finalise ...
-except sqlite3.Error as e:
-    if con:
-        con.rollback()
-    print('ERROR: {}'.format(e.args[0]))
-    sys.exit()
-finally:
-    if con:
-        con.close()
+    # Finalise ...
+    except sqlite3.Error as e:
+        if con:
+            con.rollback()
+        print('ERROR: {}'.format(e.args[0]))
+        sys.exit()
+    finally:
+        if con:
+            con.close()
+    return rows
