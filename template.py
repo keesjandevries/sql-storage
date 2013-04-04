26e7c0bd3d385318c5c1a490d9719dec578c3f23
Keesjan de Vries <keesjan777@gmail.com>
2013-04-04 17:00:58 +0100

    template script from which you can build another script

---
+++ template.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sqlite3 , argparse, sys
+import numpy as np
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Quering an sql database')
+    parser.add_argument('--input-file','-i', dest='input_file', action='store', 
+            default='test.db',  help='Name of output date base file')
+    return parser.parse_args()
+
+args=parse_args()
+
+filename=args.input_file
+
+con = None
+# the real stuff
+try:
+    #connection
+    con=sqlite3.connect(filename)
+    #cursor
+    cur=con.cursor()
+    #fetch the rows
+    cur.execute('SELECT * FROM points where CollectionId=5 and in1 <0.5 and in1> 0.3 and in2<0.001')
+
+# Finalise ...
+except sqlite3.Error as e:
+    if con:
+        con.rollback()
+    print('ERROR: {}'.format(e.args[0]))
+    sys.exit()
+finally:
+    if con:
+        con.close()
